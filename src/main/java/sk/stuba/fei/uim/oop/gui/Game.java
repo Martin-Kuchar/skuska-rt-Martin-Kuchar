82b15a209cedaf18c3e71347e39eb4230d1b895c
Martin Kuchar <martin.kcuhar97@gmail.com>
2023-05-23 13:46:59 +0200

    refractor

--- src/main/java/sk/stuba/fei/uim/oop/gui/Game.java
+++ src/main/java/sk/stuba/fei/uim/oop/gui/Game.java
@@ -10,7 +10,7 @@ public class Game {
     
     public Game() {
 
-        JFrame frame = new JFrame("RT");
+        JFrame frame = new JFrame("RT-Skuska");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,600);
@@ -87,6 +87,4 @@ public class Game {
         
         frame.setVisible(true);
     }
-
-
 }
